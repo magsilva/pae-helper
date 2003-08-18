5eb610f8e0248b5f1c9056a0f72ed564073ea8a7
marco <marco@82e6169c-43df-0310-9f88-b0bb0cb0aa5f>
2003-08-18 19:18:14 +0000

    Alterada a localiza��o dos arquivos de teste.

---
+++ tests/config.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+* Disciplina a qual será feito o controle de trabalhos.
+*/
+define( "DISCIPLINA", "Programação Orientada a Objetos" );
+
+/**
+* Email do monitor ou estagiário PAE da disciplina.
+*/
+define( "MONITOR", "magsilva@icmc.usp.br" );
+
+/**
+* Diretório a ser utilizado como repositório dos trabalhos dos alunos.
+*/
+define( "REPOSITORIO", "trabalhos" );
+
+/**
+* Arquivo contendo os dados sobre os alunos. O formato deste arquivo
+* é CSV (comma separated values), contendo os seguintes dados sobre
+* os alunos (nesta ordem): número USP, nome, senha, email.
+*/
+define( "BASE_DADOS", "alunos.txt" );
+
+/**
+* Nota mínima dos trabalhos (normalmente é 50).
+*/
+define( "NOTA_MINIMA", 50 );
+
+/**
+* Critérios para "avaliação" dos trabalhos. Se criar novos critérios,
+* acrescente aqui o respectivo nome de arquivo que o identificará.
+*/
+define( "CORRECAO_V0", "corrigido-v0.py" );
+define( "CORRECAO_V1", "corrigido-v1.py" );
+define( "CORRECAO_V2", "corrigido-v2.py" );
+define( "CORRECAO_V3", "corrigido-v3.py" );
+define( "CORRECAO_V4", "corrigido-v4.py" );
+
+?>
+
