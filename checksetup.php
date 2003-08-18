ff9671e0dccf6a014b1fb66a55cd980a5ac36091
marco <marco@82e6169c-43df-0310-9f88-b0bb0cb0aa5f>
2003-08-18 20:06:45 +0000

    Edi��o do script, retirado o pseudoc�digo.

--- checksetup.php
+++ checksetup.php
@@ -1,23 +1,31 @@
-#!/bin/php
+<?php
 
 /**
 * Configura as permissões de acesso aos arquivos vitais do sistema.
 */
+require_once( "config.php" );
 
-<?php
-	require_once( "config.php" )
+/**
+* Altera todos os arquivos para somente leitura ao dono e grupo.
+* Parâmetros: string, string, inteiro (octal), inteiro (octal).
+*/
+function alterar_permissao( $nome_diretorio, $expressao_regular, $permissao_arquivo, $permissao_diretorio ) {
+	$diretorio = opendir( $nome_diretorio );
+  while ( false !== ( $arquivo = readdir( $diretorio ) ) ) {
+  	if ( is_dir( $nome_diretorio . "/" . $arquivo ) {
+			if ( $arquivo != "." && $arquivo != ".."  ) {
+				chmod( $arquivo, $permissao_diretorio );
+				alterar_permissao( $arquivo, $expressao_regular, $permissao_arquivo, $permissao_diretorio );
+			}
+		} else {
+			if ( ereg( $expressao_regular, $arquivo ) ) {
+				chmod( $arquivo, $permissao_arquivo );
+			}
+ 		}
+	}
+}
 
-	// Altera todos os arquivos para somente leitura ao dono e grupo.
-	foreach ( $file in . ) {
-		chmod( "", 0440 );
-	}
+alterar_permissao( ".", "", 0440, 0755 );
+alterar_permissao( REPOSITORIO, "", 0440, 0755 );
 
-	// Coloca permissão de escrita no repositório.
-	chmod( REPOSITORIO ,0775 );
-	foreach ( $file.xml in REPOSITORIO ) {
-		chmod( , 0440 );
-	}
-	foreach ( $diretorio in REPOSITORIO ) {
-		chmod( $diretorio ,0775 );
-	}
 ?>
