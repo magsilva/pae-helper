ce76146b1ef531bca5839df716db2c988cc879f7
marco <marco@82e6169c-43df-0310-9f88-b0bb0cb0aa5f>
2003-08-18 20:42:29 +0000

    *** empty log message ***

---
+++ checar.php
@@ -0,0 +1,21 @@
+<?php
+	if ( !is_dir( REPOSITORIO ) ) {
+?>
+<html>
+
+<head>
+	<title>Sistema indisponível</title>
+</head>
+
+<body>
+
+<h2>Sistema indisponível</h2>
+<p>O sistema se encontra indisponível no momento. Contacte o administrador do sistema se o problema persistir.</p>
+
+</body>
+
+<html>
+<?
+		exit();
+	}
+?>
