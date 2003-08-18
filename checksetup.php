d8afa2e8fa944b46bd6d481c34528a65c491cacf
marco <marco@82e6169c-43df-0310-9f88-b0bb0cb0aa5f>
2003-08-18 19:43:50 +0000

    Esbo�ado o conte�do do script de configura��o de permiss�o de acesso aos arquivos.

--- checksetup.php
+++ checksetup.php
@@ -1,5 +1,23 @@
-chown -Rf www.root *
-chmod 0440 CHANGELOG CREDITS README TODO
-chmod 0660 *.php *.txt disk.gif
-chmod 0660 instrucoes.html
-chmod -Rf 0755 tr*
+#!/bin/php
+
+/**
+* Configura as permissões de acesso aos arquivos vitais do sistema.
+*/
+
+<?php
+	require_once( "config.php" )
+
+	// Altera todos os arquivos para somente leitura ao dono e grupo.
+	foreach ( $file in . ) {
+		chmod( "", 0440 );
+	}
+
+	// Coloca permissão de escrita no repositório.
+	chmod( REPOSITORIO ,0775 );
+	foreach ( $file.xml in REPOSITORIO ) {
+		chmod( , 0440 );
+	}
+	foreach ( $diretorio in REPOSITORIO ) {
+		chmod( $diretorio ,0775 );
+	}
+?>
